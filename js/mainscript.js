17560f6dceb4f7e6e48799ce68e9a169b74c514d
Andy Bradbury <andy@thestartupguys.co.uk>
2019-06-17 12:32:06 +0100

    add skeleton script to hide elements on page if genre doesn't match

--- js/mainscript.js
+++ js/mainscript.js
@@ -2,15 +2,27 @@ window.addEventListener('load', ()=> {
     let listName = document.getElementById("playlist");
     let allGenres = listName.getElementsByTagName("li"); // gets the full html li element as an object
     let uniqueGenres = [];
+    let selectedGenres = "hip-hop"; // TODO: Change to an array once I've implemented checkbox functionality
 
-    for(let i=0;i<allGenres.length;++i){
+    for(let i=0;i<allGenres.length;++i){ // loop through all genres in the listed element
+        // on each loop add any unique (only unique) genres fro the data tag to the new uniqueGenres array
         if(!uniqueGenres.includes(allGenres[i].getAttribute('data-genre'))){
             uniqueGenres.push(allGenres[i].getAttribute('data-genre'));
         }
-        console.log(uniqueGenres);
+
+        // TODO: Refactor into its own function so we can re-use it once checkboxes are implemented
+        // on each loop check if the genre data element for this current item matches the selected genres from the checkboxes
+        // if the genre doesn't match, hide this li element
+        if(allGenres[i].getAttribute('data-genre') !== selectedGenres) {
+            allGenres[i].style.display = "none";
+            console.log(allGenres[i].getAttribute('data-genre'));
+        }
     }
 
-    // TODO: Add functionality to only show li items matching the data-genre value of our choice. Once a static version is working I can implement the checkboxes.
+    function hideUnusedGenres() {
+
+
+    }
 
     // create a checkbox at the top of the page for each genre in the array
     // show full list by default
