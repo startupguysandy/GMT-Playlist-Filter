e65f04efca8a1fe3a97c542d3b85b38a1638d037
Andy Bradbury <andy@thestartupguys.co.uk>
2019-06-18 13:27:47 +0100

    implement functionality to hide or show list items when checkbox enabled

--- js/mainscript.js
+++ js/mainscript.js
@@ -2,19 +2,12 @@ window.addEventListener('load', ()=> {
     let listName = document.getElementById("playlist");
     let allGenres = listName.getElementsByTagName("li"); // gets the full html li element as an object
     let uniqueGenres = [];
-    let selectedGenres = []; // TODO: Change to an array once I've implemented checkbox functionality
+    let selectedGenres = [];
 
     for(let i=0;i<allGenres.length;++i){ // loop through all genres in the listed element
         // on each loop add any unique (only unique) genres fro the data tag to the new uniqueGenres array
         if(!uniqueGenres.includes(allGenres[i].getAttribute('data-genre'))){
             uniqueGenres.push(allGenres[i].getAttribute('data-genre'));
-        }
-
-        // TODO: Refactor into its own function so we can re-use it once checkboxes are implemented
-        // on each loop check if the genre data element for this current item matches the selected genres from the checkboxes
-        // if the genre doesn't match, hide this li element
-        if(allGenres[i].getAttribute('data-genre') !== selectedGenres) {
-            allGenres[i].style.display = "none";
         }
     }
 
@@ -41,23 +34,30 @@ window.addEventListener('load', ()=> {
     generateCheckboxes();
 
     function addSelectedGenre(genre) {
-        // TODO: Call this function when a checkbox on the page is checked. I think this is how I need to work with an event listener for that.
         selectedGenres.push(genre);
-        console.log(selectedGenres);
+        hideUnusedGenres(genre);
     }
 
     function removeSelectedGenre(genre) {
-        // TODO: Call this function when a checkbox on the page is checked. I think this is how I need to work with an event listener for that.
         selectedGenres.pop(genre);
-        console.log(selectedGenres);
+        showUnusedGenres(genre);
     }
 
-    function hideUnusedGenres() {
-        // TODO: Add functionality so once a checkbox is enabled we go through the DOM elements and hide anything which isn't in the selectedGenres array
-        //  I may need a button to kick off the listener initially
-    }
-    hideUnusedGenres();
+    function hideUnusedGenres(genre) {
+        // querySelectorAll grabs the information as a nodelist, need to turn it into an array to be able to access items within it
+        let itemsToHide = Array.from(document.querySelectorAll('[data-genre="'+genre+'"]'));
 
-    // show full list of genres on page load
-    // when a checkbox is checked, hide all other songs which don't match the checkbox selected
+        itemsToHide.forEach(element => {
+            element.style.display = "none";
+        });
+    }
+
+    function showUnusedGenres(genre) {
+        // querySelectorAll grabs the information as a nodelist, need to turn it into an array to be able to access items within it
+        let itemsToShow = Array.from(document.querySelectorAll('[data-genre="'+genre+'"]'));
+
+        itemsToShow.forEach(element => {
+            element.style.display = "list-item";
+        });
+    }
 });
